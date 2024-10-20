60459a7886cc9a5badddfb121ab8c9b8927b838b
fafafafaff <67899848+roqkfchqh@users.noreply.github.com>
2024-10-20 17:42:43 +0900

    [level 0] Title: 문자열 반복해서 출력하기, Time: 1.97 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ C/프로그래머스/0/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define LEN_INPUT 11
+
+int main(void) {
+    char s1[LEN_INPUT];
+    int a, num = 1;
+    scanf("%s %d", s1, &a);
+    for (; num <= a; num++)
+    printf("%s", s1);
+    return 0;
+}
