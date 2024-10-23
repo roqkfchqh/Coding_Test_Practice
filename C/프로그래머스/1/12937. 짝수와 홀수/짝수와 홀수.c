6c8e88f07c2d29c2ccec01920de75e7da40c9694
youn9 <67899848+roqkfchqh@users.noreply.github.com>
2024-10-23 19:20:29 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.01 ms, Memory: 4.45 MB -BaekjoonHub

--- C/프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.c
+++ C/프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.c
@@ -4,8 +4,8 @@
 
 char* solution(int num) {
     // 리턴할 값은 메모리를 동적 할당해주세요
-    char* answer = (char*)malloc(sizeof(char));
-    if(num == 0 || num % 2 == 0) answer = "Even";
-    else answer = "Odd";
+    char* answer = (char*)malloc(6 * sizeof(char));
+    if(num == 0 || num % 2 == 0) strcpy(answer, "Even");
+    else strcpy(answer, "Odd");
     return answer;
 }
