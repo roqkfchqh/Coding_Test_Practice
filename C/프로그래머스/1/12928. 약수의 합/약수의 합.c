f2f44a7a1a38007211aac6702ce596a65d77259b
youn9 <67899848+roqkfchqh@users.noreply.github.com>
2024-10-23 16:47:14 +0900

    [level 1] Title: 약수의 합, Time: 0.02 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ C/프로그래머스/1/12928. 약수의 합/약수의 합.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;
+    int i = 1;
+    for(; i <= n; i++){
+        if(n % i == 0) answer += i;
+    }
+    return answer;
+}
