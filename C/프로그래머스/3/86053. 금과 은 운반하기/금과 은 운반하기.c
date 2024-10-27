7b48c4e4ea1b540a277f9cad7a2f15d825b1e79a
youn9 <67899848+roqkfchqh@users.noreply.github.com>
2024-10-28 00:06:50 +0900

    [level 3] Title: 금과 은 운반하기, Time: 105.32 ms, Memory: 15.3 MB -BaekjoonHub

---
+++ C/프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+bool can_transport(long long mid, int a, int b, int g[], int s[], int w[], int t[], size_t t_len) {
+    long long total_gold = 0, total_silver = 0, total_weight = 0;
+
+    for(int i = 0; i < t_len; i++){
+        long long max_trips = mid / (2 * t[i]);
+        long long max_transport = max_trips * w[i];
+        
+        long long available_gold = (g[i] < max_transport) ? g[i] : max_transport;
+        long long available_silver = (s[i] < max_transport) ? s[i] : max_transport;
+        long long available_combined = (g[i] + s[i] < max_transport) ? g[i] + s[i] : max_transport;
+
+        if(mid >= t[i] * (2 * max_trips + 1)){
+            long long extra_transport = w[i];
+            available_gold = (g[i] < max_transport + extra_transport) ? g[i] : max_transport + extra_transport;
+            available_silver = (s[i] < max_transport + extra_transport) ? s[i] : max_transport + extra_transport;
+            available_combined = (g[i] + s[i] <= max_transport + extra_transport) ? g[i] + s[i] : max_transport + extra_transport;
+        }
+
+        total_gold += available_gold;
+        total_silver += available_silver;
+        total_weight += available_combined;
+    }
+
+    return (total_gold >= a) && (total_silver >= b) && (total_weight >= a + b);
+}
+
+long long solution(int a, int b, int g[], size_t g_len, int s[], size_t s_len, int w[], size_t w_len, int t[], size_t t_len) {
+    long long low = 0;
+    long long high = 1e15;
+    long long answer = high;
+
+    while(low <= high){
+        long long mid = (low + high) / 2;
+        if(can_transport(mid, a, b, g, s, w, t, t_len)){
+            answer = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return answer;
+}
