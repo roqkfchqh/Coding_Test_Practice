332d594116ed22f3481638ffff1b535172bd951c
youn99 <67899848+roqkfchqh@users.noreply.github.com>
2024-11-15 20:49:29 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.17 ms, Memory: 6.53 MB -BaekjoonHub

--- C/프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.c
+++ C/프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.c
@@ -4,8 +4,7 @@
 
 #define MAX 1001
 
-int hashtable[MAX] = {0};
-
+int hashtable[MAX] = {0};   //무게배열
 
 // weights_len은 배열 weights의 길이입니다.
 long long solution(int weights[], size_t weights_len) {
@@ -17,9 +16,9 @@ long long solution(int weights[], size_t weights_len) {
     
     for (int c = 100; c <= 1000; c++) {
         if (hashtable[c] > 0) {
-            answer += (long long)hashtable[c] * (hashtable[c] - 1) / 2; //같은무게
+            answer += (long long)hashtable[c] * (hashtable[c] - 1) / 2; //같은무게nC2
 
-            if(c * 4 / 3 <= 1000 && c * 4 % 3 == 0){
+            if(c * 4 % 3 == 0){
                 answer += (long long)hashtable[c] * hashtable[c * 4 / 3]; //4 3
             }
 
@@ -27,23 +26,10 @@ long long solution(int weights[], size_t weights_len) {
                 answer += (long long)hashtable[c] * hashtable[c * 2]; //4 2
             }
 
-            if (c * 3 / 2 <= 1000 && c * 3 % 2 == 0) {
+            if (c * 3 % 2 == 0) {
                 answer += (long long)hashtable[c] * hashtable[c * 3 / 2]; //3 2
             }
         }
     }
-    
-    
-    
-    /*
-    for(int i = 0; i < weights_len - 1; i++){
-        for(int j = 1; j < weights_len- i; j++){       
-            if(weights[i] == weights[j + i]) answer++;
-            else if(weights[i] * 2 == weights[j + i] || weights[i] == weights[j + i] * 2) answer++;
-            else if(weights[i] * 2 == weights[j + i] * 3 || weights[i] * 3 == weights[j + i] * 2) answer++;
-            else if(weights[i] * 4 == weights[j + i] * 3 || weights[i] * 3 == weights[j + i] * 4) answer++;
-        }
-    }
-    */
     return answer;
 }
